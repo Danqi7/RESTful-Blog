f332a1dab7947d278d677410eb5496498e89ff68
Danqi Liao <danqiliao@gmail.com>
2016-05-12 02:42:55 -0500

    added edit, delete

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var express    = require("express"),
-    mongoose   = require("mongoose"),
-    bodyParser = require("body-parser"),
-    app	       = express();
+var express         = require("express"),
+    mongoose        = require("mongoose"),
+    bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override")
+    app	            = express();
 
-mongoose.connect("mongodb://localhost/restful_blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.set('port', (process.env.PORT || 5000));
 
+mongoose.connect("mongodb://localhost/restful_blog");
 //bog schema and model
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -37,6 +39,61 @@ app.get("/blogs", function(req, res){
             console.log("ERROR");
         } else{
             res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){
+        if (err){
+            res.render("new");
+        } else{
+            res.redirect("/blogs")
+        }
+    });
+});
+
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err, foundBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs");
         }
     });
 });
