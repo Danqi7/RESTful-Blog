d7190af8607f297f684dac201d862bc001fccae1
Danqi Liao <danqiliao@gmail.com>
2016-05-12 03:02:36 -0500

    added styling

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-var express         = require("express"),
-    mongoose        = require("mongoose"),
-    bodyParser      = require("body-parser"),
-    methodOverride  = require("method-override")
-    app	            = express();
+var express          = require("express"),
+    mongoose         = require("mongoose"),
+    bodyParser       = require("body-parser"),
+    expressSanitizer = require("express-sanitizer"),
+    methodOverride   = require("method-override")
+    app	             = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -49,6 +52,7 @@ app.get("/blogs/new", function(req, res){
 
 app.post("/blogs", function(req, res){
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if (err){
             res.render("new");
@@ -79,6 +83,7 @@ app.get("/blogs/:id/edit", function(req, res){
 });
 
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if (err){
             res.redirect("/blogs");
